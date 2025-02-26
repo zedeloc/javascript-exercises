fef05ddb54a6497c2a979647db033b1caf164d11
zedeloc <desmondcole1990@gmail.com>
2025-02-25 23:38:09 -0700

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedString = '';
+    for (let i = str.length; i > 0; i--) {
+        reversedString += str.slice(i - 1, i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
