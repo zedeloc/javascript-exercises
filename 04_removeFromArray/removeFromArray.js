b305bf2a8179ac8e8157523bc4682fca664d38e9
zedeloc <desmondcole1990@gmail.com>
2025-02-26 04:21:41 -0700

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(origValues, toRemove) {
+    let newArray = [];
+    newArray = origValues;
+    let args = Array.from(arguments).slice(1);
+    for (arg of args) {
+        for (let i = (newArray.length - 1); i >= 0; i--) {
+            if (newArray[i] === arg) {
+                newArray.splice(i, 1);
+            }
+        }
+
+    }
+    return newArray;
+} 
+    
+    
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
