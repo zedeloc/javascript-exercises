39afa82e8a68d1ed04deb1ef08eb61a222c95bd7
zedeloc <desmondcole1990@gmail.com>
2025-02-26 04:47:58 -0700

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let big;
+    let small;
+    let sum = 0;
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR'
+    } else if (a !== Math.floor(a) || b !== Math.floor(b)) {
+        return 'ERROR'
+    } else if (a < 0 || b < 0) {
+        return 'ERROR'
+    }
+
+    if (a > b) {
+        big = a;
+        small = b;
+    } else {
+        big = b;
+        small = a
+    }
+
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
